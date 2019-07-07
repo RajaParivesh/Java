0845e6b1f30d9efc1171771de81646054dbe35d2
Raja Parivesh <rajaparivesh@gmail.com>
2019-07-07 23:08:16 +0530

    Reverse interger using recursion

--- src/collections/Main.java
+++ src/collections/Main.java
@@ -1,27 +1,29 @@
 package collections;
 
+import java.util.Scanner;
+
 public class Main {
     /**
      * fib using recursion
      * @param n
      * @return
      */
-    /*private int fib(int n){
+    private int fib(int n){
         //Base case
         if(n==1) {
-            System.out.println(0);
+//            System.out.println(0);
             return 0;
         }
         if(n==2) {
-            System.out.println(1);
+//            System.out.println(1);
             return 1;
         }
         // Recursive case
         int x;
-        x = fib(n-1);      // + fib(n-2);
+        x = fib(n-1);     // + fib(n-2);
         System.out.println(x);
         return x;
-    }*/
+    }
 
     /**
      * Factorial Using recursion
@@ -37,10 +39,38 @@ public class Main {
         return x;
     }
 
+//    private int permutation(String s, String remaining){
+//        return
+//    }
+//
+    //A method for reverse
+    public static void reverseMethod(int number) {
+        if (number < 10) {
+            System.out.println(number);
+            return;
+        }
+        else {
+            System.out.print(number % 10);
+            //Recursion
+            reverseMethod(number/10);
+        }
+    }
 
     public static void main(String args[]){
         Main obj = new Main();
-//        obj.fib(3);
-        System.out.println(obj.factorial(5));
+        //fib not working
+//        System.out.println(obj.fib(4));
+//        System.out.println(obj.factorial(5));
+//        obj.reverse(124);
+
+        // For Reverse integer function
+        int num=0;
+        System.out.println("Input your number and press enter: ");
+        Scanner in = new Scanner(System.in);
+        num = in.nextInt();
+        System.out.print("Reverse of the input number is:");
+        reverseMethod(num);
+
     }
+
 }
