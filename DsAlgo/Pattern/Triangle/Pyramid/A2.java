051a223febc3912ebaf9fadae9403e9d0468e237
Raja Parivesh <rajaparivesh@gmail.com>
2021-02-06 22:05:31 +0530

    Printed Pyramid triangle with spaces

--- DsAlgo/Pattern/Triangle/Pyramid/A2.java
+++ DsAlgo/Pattern/Triangle/Pyramid/A2.java
@@ -1,14 +1,14 @@
 //   1 2 3 4 5 6 7 8 9
 // 1         *
-// 2       * * *
-// 3     * * * * * 
-// 4   * * * * * * * 
-// 5 * * * * * * * * *
+// 2       *   *
+// 3     *   *   * 
+// 4   *   *   *   * 
+// 5 *   *   *   *   *
 
 
 import java.util.Scanner;
 
-public class A1 {
+public class A2 {
 
     public static void main(String[] args) {
         
@@ -17,14 +17,16 @@ public class A1 {
         System.out.println("\nPlease enter a number: ");
         int n = s.nextInt();
         
-        for(int i=1; i<=n; i++){    
-            for(int j=n-i; j>0; j--){   
+        for(int i=1; i<=n; i++){
+            for(int j=n-i; j>0; j--){
                 System.out.print("  ");
             }
-            for(int j = 1 ; j<=2*i-1; j++){       
-                System.out.print("* ");
+            for (int j = 0; j < i; j++) {
+                System.out.print("*   ");                
             }
             System.out.println();
         }
+
+      
     }
 }
