42f866eeaf72f2cb70e9e56276a9512674387824
raja <raja@erpsolutions06.onmicrosoft.com>
2021-03-07 19:08:56 +0530

    FirstNonRepeatingCharacter amazon asked question: solved by method2

--- DsAlgo/Amazon/FirstNonRepeatingCharacter.java
+++ DsAlgo/Amazon/FirstNonRepeatingCharacter.java
@@ -5,24 +5,35 @@ import java.lang.Error;
 
 public class FirstNonRepeatingCharacter {
 
-    public static void firstNonRepeatingCharacter(String str) {
-        HashMap<Character, Integer> h = new HashMap<>();
-        char key;
+    //Method 1
+    // public static void firstNonRepeatingCharacter(String str) {
+    //     HashMap<Character, Integer> h = new HashMap<>();
+    //     char key;
+    //     for (int i = 0; i < str.length(); i++) {
+    //         key = str.charAt(i);
+    //         if (h.containsKey(key))
+    //             h.put(key, h.get(key) + 1);
+    //         else
+    //             h.put(key, 1);
+    //     }
+    //     for (int i = 0; i < str.length(); i++) {
+    //         key = str.charAt(i);
+    //         if (h.get(key) == 1) {
+    //             System.out.println(key);
+    //             break;
+    //         }
+    //     }
+    // }
+
+    //Method 2
+    private static void firstNonRepeatingCharacter(String str) {
         for (int i = 0; i < str.length(); i++) {
-            key = str.charAt(i);
-            if (h.containsKey(key))
-                h.put(key, h.get(key) + 1);
-            else
-                h.put(key, 1);
-        }
-        for (int i = 0; i < str.length(); i++) {
-            key = str.charAt(i);
-            if (h.get(key) == 1) {
+            char key = str.charAt(i);
+            if(str.lastIndexOf(key)== str.indexOf(key)){
                 System.out.println(key);
-                break;
+                return;
             }
         }
-        
     }
 
     public static void main(String args[]) {
@@ -30,5 +41,4 @@ public class FirstNonRepeatingCharacter {
         String str = s.next();
         firstNonRepeatingCharacter(str);
     }
-
 }
